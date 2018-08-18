b00b7c894d27e9a046f382b60f3626ba25d062bb
Toluwani <abiodun.toluwanii@gmail.com>
2018-08-18 11:57:39 -0700

    'gtgh'

--- api_auth/views.py
+++ api_auth/views.py
@@ -24,7 +24,7 @@ def login(request):
     username = request.data.get("username")
     password = request.data.get("password")
     if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
+        return Response({'message': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if not user:
